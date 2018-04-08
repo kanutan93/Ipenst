99f9a4b0a968b4d8e550624a35bc60eef6274435
kanutan93 <Ucdklve2>
2018-04-08 11:49:54 +0700

    header scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './containers/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ButtonComponent } from './components/button/button.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ButtonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,19 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent implements OnInit {
+  @Input() text: string;
+  @Output() click: EventEmitter<any> = new EventEmitter<any>();
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onClick() {
+    this.click.emit();
+  }
+
+}
