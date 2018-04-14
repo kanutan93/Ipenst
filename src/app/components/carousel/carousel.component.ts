5fce925f1e0d2bc23fba5556bdfcb2ce4cba8c17
kanutan93 <kanutan93@gmail.com>
2018-04-14 20:02:53 +0700

    solutions template

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-carousel',
@@ -11,6 +12,7 @@ export class CarouselComponent implements OnInit {
       title: 'Аудит информационной безопасности',
       description: 'Комплексный аудит информационной безопасности' +
       ' - первый шаг к построению качественной системы защиты',
+      routerLink: '/solutions/audit',
       src: '5342c1f29d80cfd96d069c31006c5727.png',
       background: 'ee06cfa1ae1c1201445567a85a77e9be.png'
     },
@@ -19,6 +21,7 @@ export class CarouselComponent implements OnInit {
       description: 'Предлагаем услугу хостинга сайта в центре обработки данных,' +
       ' аттестованном в соответствии с требованиями безопасности,' +
       ' предъявляемым к государственным информационным системам',
+      routerLink: '/solutions/hosting',
       src: '640d348aeb76fba582ccd9f7338f6ef6.png',
       background: 'a8bbaf67e7dced2f0bad75e32240f00c.png'
     },
@@ -27,13 +30,18 @@ export class CarouselComponent implements OnInit {
       description: 'Набор бизнес-практик, ' +
       'включающих управление и оптимизацию закупок, внедрения, поддержки,' +
       ' использования и вывода из эксплуатации программных приложений внутри организации',
+      routerLink: '/solutions/sam',
       src: 'cdd7f95707dbc678399ff9717234c684.png',
       background: 'fb90adaf959a1a7384048f20e5e8468a.png'
     }
   ];
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onSolution(routerLink) {
+    this.router.navigate([routerLink]);
   }
 
 }
--- src/app/containers/home/home.component.ts
+++ src/app/containers/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {a} from '@angular/core/src/render3';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -12,35 +13,41 @@ export class HomeComponent implements OnInit {
     {
       imgSrc: '1.jpg',
       title: 'Тест на проникновение',
+      routerLink: '/solutions/penetration',
       description: 'Тесты на проникновение мы проводим, используя' +
       ' OSSTMM методику для соответствия качества работ международным стандартам'
     },
     {
       imgSrc: '2.jpg',
       title: 'Защита АСУТП',
+      routerLink: '/solutions/asutp',
       description: 'В рамках построения проектов по защите КСИИ' +
       ' и АСУТП работы  проводятся в соответствии с требованиями законодательства'
     },
     {
       imgSrc: '3.jpg',
       title: 'Аудит',
+      routerLink: '/solutions/audit',
       description: 'Комплексный аудит для определения актуальных' +
       ' рисков информ безопасности и формирование плана работ по их снижению'
     },
     {
       imgSrc: '4.jpg',
       title: 'Сетевая безопасность',
+      routerLink: '/solutions/security',
       description: 'Построение систем сетевой безопасности под ключ – от аудита и проектирования, до построения ее комплексной системы'
     },
     {
       imgSrc: '5.jpg',
       title: 'SAM',
+      routerLink: '/solutions/sam',
       description: 'Методология, объединяющая эффективные технологии ' +
       'и процедуры управления программными активами и оптимизации их использования'
     },
     {
       imgSrc: '6.jpg',
       title: 'Аттестованный хостинг',
+      routerLink: '/solutions/hosting',
       description: 'Выполнение специального аудиторского задания' +
       ' по рассмотрению бухгалтерских и налоговых отчетов экономического субъекта.'
     },
@@ -55,9 +62,13 @@ export class HomeComponent implements OnInit {
       title: 'Омскгражданпроект защищает данные с помощью Falcongaze SecureTower'
     }
   ]
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onNews() {
+    this.router.navigate(['/news']);
   }
 
 }
