7352cd20dea9f7067b1e5513bef951a58e2218b2
kanutan93 <kanutan93@gmail.com>
2018-04-16 01:16:07 +0700

    heroku

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const bodyParser = require('body-parser');
 var client = nodemailer.createTransport({
   service: 'SendGrid',
   auth: {
-    user: 'app93885195@heroku.com',
-    pass: '5yxs0qe99385'
+    user: 'app93894600@heroku.com',
+    pass: 'q3qur7le2085'
   }
 });
 
@@ -40,7 +40,7 @@ app.post('/api/v1', (req, res) => {
   msg += `<h2>Сумма: <span style="color: green">${req.body.sum}</span></h2>`;
 
   const messageClient = {
-    from: 'app93885195@heroku.com',
+    from: 'app93894600@heroku.com',
     to: 'kanutan93@gmail.com',
     subject: 'Prototype. Заявка на тестирование.',
     html: msg
@@ -56,7 +56,9 @@ app.post('/api/v1', (req, res) => {
 });
 
 app.use('/', express.static(__dirname + '/dist'));
-
+app.all('*', (req, res) =>  {
+  res.redirect(`https://ipsecurity.herokuapp.com/`);
+});
 app.listen(port, () => {
   console.log('server successfully started on port '+ port);
 });
