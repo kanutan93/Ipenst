dd58bc8ea16001228dd71252e402eab3dee8f028
kanutan93 <kanutan93@gmail.com>
2018-04-15 09:32:12 +0700

    to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor() {
+
+    const scrollFunction = () => {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById('scrollToTop').style.opacity = '1';
+      } else {
+        document.getElementById('scrollToTop').style.opacity = '0';
+      }
+    };
+    window.onscroll = scrollFunction;
+  }
+
+  topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 }
